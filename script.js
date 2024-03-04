0162a6b5cf4554822351b88731f2669aedead108
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-04 12:20:52 +0530

    Create functions for basic maths

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create functions for basic operations: add, subtract, multiply, divide
+
+// sum
+// any number of arguments
+// sum of all arguments
+function sum(...args) {
+  let sum = 0;
+  for (arg of args) {
+    sum += arg;
+  }
+  return sum;
+}
+
+// multiplication
+function multiply(...args) {
+  let product = 1;
+  for (arg of args) {
+    product *= arg;
+  }
+  return product;
+}
+
+// subtraction
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
