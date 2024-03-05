291f41e8d058125d584d3b1c31f865a1bfadc675
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-05 06:33:09 +0530

    Store buttons DOM objects in variables

--- script.js
+++ script.js
@@ -52,3 +52,34 @@ function operate(operandA, operandB, operator) {
     return operation(operandA, operandB);
   }
 }
+
+// Create the functions that populate the display when you
+// click the number buttons. You should be storing the
+// ‘display value’ in a variable somewhere for use in the next step.
+
+// Get the DOM object for all the calculator keys
+
+// digits
+const digit1 = document.querySelector(".digit-1");
+const digit2 = document.querySelector(".digit-2");
+const digit3 = document.querySelector(".digit-3");
+const digit4 = document.querySelector(".digit-4");
+const digit5 = document.querySelector(".digit-5");
+const digit6 = document.querySelector(".digit-6");
+const digit7 = document.querySelector(".digit-7");
+const digit8 = document.querySelector(".digit-8");
+const digit9 = document.querySelector(".digit-9");
+const digit0 = document.querySelector(".digit-0");
+const digit00 = document.querySelector(".digit-00");
+const digitDot = document.querySelector(".digit-dot");
+
+// calcullations
+const btnEq = document.querySelector(".btn-eq");
+const btnAdd = document.querySelector(".btn-add");
+const btnSubtract = document.querySelector(".btn-subtract");
+const btnMultiply = document.querySelector(".btn-multiply");
+const btnDivide = document.querySelector(".btn-divide");
+
+// clear
+const btnAllClear = document.querySelector(".btn-allclear");
+const btnClear = document.querySelector(".btn-clear");
